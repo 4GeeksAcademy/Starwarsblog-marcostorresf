0815f97770588658e2246396d8bc5a559da3819d
marcostorresf <marcos_e19@icloud.com>
2025-04-11 23:36:25 +0000

    doing my best

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,17 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1"><img src="https://img.icons8.com/?size=100&id=38539&format=png&color=000000" /></span>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button class="btn btn-primary btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							FAVORITES
+						</button>
+						<ul class="dropdown-menu">
+							...
+						</ul>
+
 					</Link>
 				</div>
 			</div>
